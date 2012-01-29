23aa4733a361a67af0f5ee0816aa826c75270de0
Joshua Hawxwell <m@hawx.me>
2012-01-29 11:14:13 +0000

    Version bump to 0.3.0

--- lib/guard/shell.rb
+++ lib/guard/shell.rb
@@ -4,26 +4,26 @@ require 'guard/watcher'
 
 module Guard
   class Shell < Guard
-  
-    VERSION = '0.2.0'
-    
+
+    VERSION = '0.3.0'
+
     # Calls #run_all if the :all_on_start option is present.
     def start
       run_all if options[:all_on_start]
     end
-    
+
     # Call #run_on_change for all files which match this guard.
     def run_all
       run_on_change(Watcher.match_files(self, Dir.glob('{,**/}*{,.*}').uniq))
     end
 
-    # Print the result of the command, if there is a result to be printed.
+    # Print the result of the command(s), if there are results to be printed.
     def run_on_change(res)
       puts res if res
     end
-    
+
   end
-  
+
   class Dsl
     # Easy method to display a notification
     def n(msg, title='')

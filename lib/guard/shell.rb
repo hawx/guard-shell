f8340e45fbe6f4a09623f4c84149dde8279b4d31
Joshua Hawxwell <m@hawx.me>
2012-03-09 15:01:31 +0000

    Version bump to 0.4.0

--- lib/guard/shell.rb
+++ lib/guard/shell.rb
@@ -5,7 +5,7 @@ require 'guard/watcher'
 module Guard
   class Shell < Guard
 
-    VERSION = '0.3.1'
+    VERSION = '0.4.0'
 
     # Calls #run_all if the :all_on_start option is present.
     def start
